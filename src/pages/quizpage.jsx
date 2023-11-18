4dd6f9d230f3c69c69d6a2156a17415cf2045e60
HapaxHypatia <r.haskmann@gmail.com>
2023-11-18 22:22:59 +1000

    working on scoring function

--- src/pages/quizpage.jsx
+++ src/pages/quizpage.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import './quizpage.css'
 import questions from "../questions.json";
 
 function Quizpage() {
+	const [score, setScore] = useState(0)
+	const [response, setResponse] = useState([])
 
 	const parsing = [
 		'singular nominative',
@@ -17,10 +19,10 @@ function Quizpage() {
 		'plural dative',
 		'singular ablative',
 		'plural ablative']
-	const options = parsing.map((answer, index)=>
+	const options = parsing.map((option, index)=>
 		<label className={'option'} id={index}>
-			<input type={"checkbox"} name={index}></input>
-			<span>{answer}</span>
+			<input type={"checkbox"} onChange={changeResponse} name={option}></input>
+			<span>{option}</span>
 		</label>
 		)
 
@@ -30,21 +32,40 @@ function Quizpage() {
 	const allAnswers = questions.filter(q => q.Ending === question).map(q => q.Parsing)
 	const answers = [...new Set(allAnswers)]
 
+	function changeResponse(e){
+		if (e.target.checked)
+			setResponse([e.target.name, [...response]])
+	}
+
 	function check (e){
 		e.preventDefault()
+		let points = 0
+		for (let item of response){
+			if (item in answers){
+				points++
+			}
+			else{
+				points--
+			}
+		}
+		setScore(score+points)
+	}
 
+	function reset(){
+		setScore(0)
 	}
 
 	return (
 		<div id={'main'}>
 			<p>Select all the options for the following ending. Note that macrons are not used in this quiz</p>
+			<p>{score}</p>
+			<button onClick={reset}>Reset</button>
 			<div id={'questioncontainer'}>
 				<div className={'question'}>{question}</div>
 				<form>
 					<div className={'answerbox'}>{options}</div>
-					<button onSubmit={check}>Submit</button>
+					<button onClick={check}>Submit</button>
 				</form>
-
 			</div>
 
 
